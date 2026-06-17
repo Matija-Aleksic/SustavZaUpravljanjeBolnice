532fa0c5ec38a23d67ba0a8a2564d935818904d5
alex <matijaaleksic22@gmail.com>
2026-06-17 17:43:24 +0200

    priming for login logic

---
+++ src/main/java/com/alex/sustavzaupravljanjebolnice/util/UserSession.java
@@ -0,0 +1,30 @@
+package com.alex.sustavzaupravljanjebolnice.util;
+
+import com.alex.sustavzaupravljanjebolnice.entity.Staff;
+
+public class UserSession {
+    private static UserSession instance;
+    private Staff loggedInStaff;
+
+    private UserSession() {
+    }
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public Staff getLoggedInStaff() {
+        return loggedInStaff;
+    }
+
+    public void setLoggedInStaff(Staff staff) {
+        this.loggedInStaff = staff;
+    }
+
+    public void cleanUserSession() {
+        this.loggedInStaff = null;
+    }
+}
